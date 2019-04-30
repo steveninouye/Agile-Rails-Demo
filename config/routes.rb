ef22c6e1305951905fea947a53307bbce4574a71
Steven Inouye <steveninouye@msn.com>
2019-04-29 20:50:49 -0700

    Render products on the story#index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
+  root 'store#index', as: 'store_index'
   resources :products
-  get 'say/hello'
-  get 'say/goodbye'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
