c7be3355ea4faea156664887e4be7987b9dd4e96
Steven Inouye <steveninouye@msn.com>
2019-05-11 19:58:18 -0700

    Add translation from english to spanish

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,5 +1,5 @@
 class OrderMailer < ApplicationMailer
-  default from: 'Sam Ruby <steveninouye808@gmail.com>'
+  default from: 'Sam Ruby <depot@example.com>'
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
